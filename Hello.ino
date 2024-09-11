c7c2adfbedfd3b4b370e66b5abc5a9fed18ef726
J.S.LI <jsli@linux.com>
2024-09-11 15:56:29 +0800

    feat: make it work on macos(and probably also linux)

--- Hello.ino
+++ Hello.ino
@@ -1,16 +1,16 @@
-#define LED_BUILTIN PIN_P0_24 //Red, Low Active
-//#define LED_BUILTIN PIN_P0_25 //Green, Low Active
-//#define LED_BUILTIN PIN_P0_26 //Blue, Low Active
+#define LED_RED PIN_P0_24 //Red, Low Active
+#define LED_GREEN PIN_P0_25 //Green, Low Active
+#define LED_BLUE PIN_P0_26 //Blue, Low Active
 
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LED_BLUE, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
+  digitalWrite(LED_BLUE, HIGH);  // turn the LED on (HIGH is the voltage level)
   delay(1000);                      // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
+  digitalWrite(LED_BLUE, LOW);   // turn the LED off by making the voltage LOW
   delay(1000);                      // wait for a second
 }
--- Makefile
+++ Makefile
@@ -7,3 +7,13 @@ setup:
 
 board:
 	arduino-cli board list
+
+build:
+	arduino-cli compile -b InstantFind:InstantFind:ITM9100DK
+
+serial:
+	arduino-cli monitor -p /dev/cu.usbmodem0001A00000001
+
+upload: build
+	# arduino-cli upload -p /dev/cu.usbmodem0001A00000001 -b InstantFind:InstantFind:ITM9100DK 
+	arduino-cli upload -b InstantFind:InstantFind:ITM9100DK 
